6673eb8fcbdb6cf3c1265188962f599160d4198e
narishige <narishige36@gmail.com>
2020-12-11 11:04:24 +0900

    追加

--- wpsave.php
+++ wpsave.php
@@ -28,3 +28,25 @@ Template Name: no-sidebar（カスタムテンプレートの名前）
 
 記事URL取得
 <?php the_permalink(); ?>
+
+タイトル文字制限
+<?php
+if(mb_strlen($post->post_title, 'UTF-8')>20){
+	$title= mb_substr($post->post_title, 0, 20, 'UTF-8');
+	echo $title.'……';
+}else{
+	echo $post->post_title;
+}
+?>
+
+ワードプレスのユーザー画像を表示
+<?php
+$author = get_the_author_meta( 'id' );
+$author_img = get_avatar( $author );
+$imgtag = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
+if ( preg_match( $imgtag, $author_img, $imgurl ) ) {
+$authorimg = $imgurl[ 2 ];
+}
+?>
+画像表示
+<img src="<?php echo $authorimg ?>" alt="">
