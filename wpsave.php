21293ab0719a9a0b7680ef9085eeeec9eef82a55
narishige <narishige36@gmail.com>
2021-02-22 16:22:28 +0900

    カスタムフィールについて追記

--- wpsave.php
+++ wpsave.php
@@ -1,39 +1,38 @@
-カスタム固定ページ表示
+<!-- カスタム固定ページ表示 -->
 <?php
 /*
 Template Name: no-sidebar（カスタムテンプレートの名前）
 */
 ?>
 
-トップURL取得
+<!-- トップURL取得 -->
 <?php echo home_url() ?>
 
-カテゴリーID6を10個表示
-ループ前に記載
+<!-- カテゴリーID6を10個表示
+ループ前に記載 -->
 <?php query_posts('cat=6&posts_per_page=10'); ?>
 
-ワードプレスループ関数
+<!-- ワードプレスループ関数 -->
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		/* ここに関数等を記載 */
+		<!-- ここに関数等を記載 -->
 <?php endwhile;
 endif; ?>
 
-投稿者名表示
+<!-- 投稿者名表示 -->
 <?php the_author(); ?>
 
-カテゴリー名表示
+<!-- カテゴリー名表示 -->
 <?php the_category(); ?>
-
-日付表示
+<!-- 日付表示 -->
 <?php echo get_the_date('Y.n.d'); ?>
 
-トップページのURL表示
+<!-- トップページのURL表示 -->
 <?php echo home_url() ?>
 
-記事URL取得
+<!-- 記事URL取得 -->
 <?php the_permalink(); ?>
 
-タイトル文字制限
+<!-- タイトル文字制限 -->
 <?php
 if (mb_strlen($post->post_title, 'UTF-8') > 20) {
 	$title = mb_substr($post->post_title, 0, 20, 'UTF-8');
@@ -43,7 +42,7 @@ if (mb_strlen($post->post_title, 'UTF-8') > 20) {
 }
 ?>
 
-ワードプレスのユーザー画像を表示
+<!-- ワードプレスのユーザー画像を表示 -->
 <?php
 $author = get_the_author_meta('id');
 $author_img = get_avatar($author);
@@ -52,13 +51,13 @@ if (preg_match($imgtag, $author_img, $imgurl)) {
 	$authorimg = $imgurl[2];
 }
 ?>
-画像表示
+<!-- 画像表示 -->
 <img src="<?php echo $authorimg ?>" alt="">
 
-別のphpファイルから読み込み
+<!-- 別のphpファイルから読み込み -->
 <?php include("aaaaa.php"); ?>
 
-ナビゲーションメニューをループで表示
+<!-- ナビゲーションメニューをループで表示 -->
 <?php
 $menu_items = wp_get_nav_menu_items('メニュー名を入力');
 if ($menu_items) :
@@ -72,19 +71,18 @@ endif;
 wp_reset_postdata();
 ?>
 
-アドバンスカスタムフィールド
-テキスト
+<!-- アドバンスカスタムフィールド
+テキスト -->
 <?php the_field(); ?>
 
-画像
+<!-- 画像 -->
 <?php
 get_field($contentsimg);
 if ($images) {
 	echo '<img src="' . $images[url] . '" >';
 }
 ?>
-
-例
+<!-- 例 -->
 					<?php for ($i = 1; $i <= 4; $i++) :
 						$contentsimg = "contents0" . $i . "_img";
 						$contentstext = "contents0" . $i . "_title";
@@ -104,4 +102,14 @@ if ($images) {
 								<?php endif; ?>
 								<h2><?php the_field($contentstext); ?></h2>
 							</a>
-						</li>
+</li>
+
+<!-- function.phpに記載してページで呼び出す -->
+<?php
+function get_shop_info() {
+     $myshop = get_field('shop_info',123);
+     return $myshop;
+}
+add_shortcode('show_shop_info', 'get_shop_info');
+
+?>
